82e5038ed5da14bec52ea693a9c339864c1d19c7
Dylan K. Taylor <dktapps@pmmp.io>
2023-03-29 19:24:58 +0100

    Added script to generate timings HTML from a CLI

---
+++ generate-report-cli.php
@@ -0,0 +1,15 @@
+<?php
+
+use Starlis\Timings\LegacyHandler;
+
+require 'vendor/autoload.php';
+
+if(count($argv) < 3){
+	echo "Usage: php generate-report-cli.php <input file> <output file>\n";
+	exit(1);
+}
+
+$raw = file_get_contents($argv[1]);
+ob_start();
+LegacyHandler::load($raw);
+file_put_contents($argv[2], ob_get_clean());

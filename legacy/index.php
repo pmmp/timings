dabbd770e01c0421200caeda37d5bfa35effd711
Dylan K. Taylor <dktapps@pmmp.io>
2023-03-31 18:59:43 +0100

    Improve dialog styling

--- legacy/index.php
+++ legacy/index.php
@@ -413,12 +413,12 @@ ROW;
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>PocketMine-MP Timings Viewer</title>
-		<link rel="stylesheet" href="legacy/timings.css"/>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 		<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/themes/smoothness/jquery-ui.css"/>
 		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js"></script>
 		<script src="legacy/timings.js"></script>
 		<meta name="robots" content="noindex">
+		<link rel="stylesheet" href="legacy/timings.css"/>
 	</head>
 	<body>
 		<div class="pageHeader">

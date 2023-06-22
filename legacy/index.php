b03c689d4a97468615e64db5c7b56f9bf367cd53
Dylan K. Taylor <dktapps@pmmp.io>
2023-06-22 11:57:43 +0100

    Always access jquery static files using https

--- legacy/index.php
+++ legacy/index.php
@@ -239,9 +239,9 @@ ROW;
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>PocketMine-MP Timings Viewer</title>
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-		<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/themes/smoothness/jquery-ui.css"/>
-		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/themes/smoothness/jquery-ui.css"/>
+		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js"></script>
 		<script src="static/js/timings.js"></script>
 		<meta name="robots" content="noindex">
 		<link rel="stylesheet" href="static/css/timings.css"/>

64002b8bc23c8dd60b21d4a0079d652891b16de8
Dylan K. Taylor <dktapps@pmmp.io>
2024-11-16 14:05:51 +0000

    generate-report-cli now works when invoked from other working directory

--- generate-report-cli.php
+++ generate-report-cli.php
@@ -2,7 +2,7 @@
 
 use Starlis\Timings\LegacyHandler;
 
-require 'vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 if(count($argv) < 3){
 	echo "Usage: php generate-report-cli.php <input file> <output file>\n";
